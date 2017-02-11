6fae9fc7f80ae4fb24cb064ade6bfe717da4a5ba
David McFadzean <davidmc@gmail.com>
2017-02-11 17:43:31 -0500

    Added sessions and transactions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"fmt"
+	"time"
 	"strconv"
 	"io/ioutil"
 	"path/filepath"
@@ -11,7 +12,13 @@ import (
 
 	"goji.io"
 	"goji.io/pat"
+	
+	"github.com/google/uuid"
 )
+
+var datapath string
+var sessionID uuid.UUID
+var sessionPath string
 
 type Config struct {
 	Port int
@@ -47,23 +54,42 @@ func (config *Config) Read(filepath string) {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	txID := uuid.New()
+	txPath := filepath.Join(sessionPath, "txs", txID.String())
+	os.MkdirAll(txPath, os.ModePerm)
+	reqpath := filepath.Join(txPath, "request")
+	f, _ := os.Create(reqpath)
+	r.Write(f)
+	f.Close()
+
 	fmt.Fprintf(w, "index")
 }
 
 func main() {
 
-	datapath := filepath.Join(os.Getenv("GOPATH"), "data") 
-	err := os.Mkdir(datapath, os.ModePerm)
-    fmt.Println(datapath, err)
+	datapath = filepath.Join(os.Getenv("GOPATH"), "data") 
+	err := os.MkdirAll(datapath, os.ModePerm)
+	fmt.Println(datapath, err)
 	
-	configpath := filepath.Join(datapath, "config.json")
+	configPath := filepath.Join(datapath, "config.json")
 	config := Config{8000}
-	config.Read(configpath)
+	config.Read(configPath)
+	
+	sessionID = uuid.New()
+	sessionPath = filepath.Join(datapath, "sessions", sessionID.String())
+	err = os.MkdirAll(sessionPath, os.ModePerm)
+	fmt.Println("session:", sessionID, sessionPath, err)
+	
+	logPath := filepath.Join(datapath, "sessions", "log")
+	log, _ := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	//log.WriteString(sessionID.String())
+	fmt.Fprintf(log, "%v|%v\n", time.Now().UTC(), sessionID)
+	log.Close()
 	
 	addr := "localhost:" + strconv.Itoa(config.Port)
 	
 	mux := goji.NewMux()
-	mux.HandleFunc(pat.Get("/"), index)
+	mux.HandleFunc(pat.Get("/*"), index)
 
 	fmt.Println("Starting alter-ego on ", addr)
 	http.ListenAndServe(addr, mux)
